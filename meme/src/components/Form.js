1a5a4b8b4403e9e6c151dc6d3ff960105fc3ed0f
Gaurang-Gaur <gauranggaur007@gmail.com>
2023-08-26 22:10:38 +0530

    working

--- meme/src/components/Form.js
+++ meme/src/components/Form.js
@@ -1,49 +1,41 @@
 import React from "react";
 import memeData from "../memeData.js";
 
-export default function Form(){
-let url;
+export default function Form() {
+  let url;
 
-const meme={
-    topText:"",
-    bottomText:"",
-    randomImage:"http://i.imgflip.com/1bij.jpg"
-}
+  const [meme, setmeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-// console.log(typeof(memeData));
-const [allMemeImages, setallMemeImages]=React.useState(memeData);
-    
- const [img,setimg]=React.useState("");
- 
-    function generaterandomno(){
-      const meme=   allMemeImages.data.memes;
-    
-    let randmemeobj;
-    randmemeobj=meme[Math.floor(Math.random()*meme.length)];
-    
-//    const {url}=randmemeobj;// This is destructuring of the object in js which equal to randmemeobj.url
-   url=randmemeobj.url;
+  const [allMemeImages, setallMemeImages] = React.useState(memeData);
+
+  function generaterandomno() {
+    const memeArray = allMemeImages.data.memes;
+    let randmemeobj = memeArray[Math.floor(Math.random() * memeArray.length)];
 
 
-    setimg(url);
-    
-    
+    url = randmemeobj.url; //const {url}=randmemeobj;// This is destructuring of the object in js which equal to randmemeobj.url
+    setmeme((prevmeme)=>{
+        return {
+            ...prevmeme,
+            randomImage:url
+        }
+    })
+  }
 
-
-
-
-}
-
-
-    return (
-        <div className="main">
-            <div className="form">
-                
-                <input type="text"className="form--text" placeholder="top-text"/ >
-                <input type="text"className="form--text"placeholder="bottom-text"/>
-                <button className="form--button" onClick={generaterandomno}>Get new meme image 🎇</button>
-            </div>
-            <img src={img} className="memeimg"/>
-        </div>
-    )
+  return (
+    <div className="main">
+      <div className="form">
+        <input type="text" className="form--text" placeholder="top-text" />
+        <input type="text" className="form--text" placeholder="bottom-text" />
+        <button className="form--button" onClick={generaterandomno}>
+          Get new meme image 🎇
+        </button>
+      </div>
+      <img src={meme.randomImage} className="memeimg" />
+    </div>
+  );
 }
