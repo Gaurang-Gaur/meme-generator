031a407b12541aeaa564e9f5bc41477124cc2810
Gaurang-Gaur <gauranggaur007@gmail.com>
2023-09-10 20:25:05 +0530

    complete project

--- meme/src/components/Form.js
+++ meme/src/components/Form.js
@@ -1,42 +1,105 @@
 import React from "react";
-import memeData from "../memeData.js";
+// import memeData from "../memeData.js";
+
 
 export default function Form() {
   let url;
+  let memeData;
+
+  // fetching api using the fetch method and handling side effect with use effect
+
+ 
 
   const [meme, setmeme] = React.useState({
     topText: "",
     bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg",
+    randomImage: "",
   });
 
-  const [allMemeImages, setallMemeImages] = React.useState(memeData);
 
-  function generaterandomno() {
-    const memeArray = allMemeImages.data.memes;
 
-    let randmemeobj = memeArray[Math.floor(Math.random() * memeArray.length)];
+  const [allMemes, setallMemes] = React.useState([]);
+
+
+  React.useEffect(()=>{
+    fetch("https://api.imgflip.com/get_memes").then((responds)=>
+      responds.json()
+      
+       ).then((jsonRes)=>{
+             
+       
+        memeData=jsonRes.data.memes;
+
+         return (setallMemes(memeData))
+
+      })  },[])
+
+
+  function getMemeImage() {
+  
+
+  let randmemeobj = allMemes[Math.floor(Math.random() * allMemes.length)];
 
 // here we using the seperetor operator....
-    url = randmemeobj.url; //const {url}=randmemeobj;// This is destructuring of the object in js which equal to randmemeobj.url
+    url = randmemeobj.url; 
+    
+                //const {url}=randmemeobj;// This is destructuring of the object in js which equal to randmemeobj.url
     setmeme((prevmeme)=>{
         return {
             ...prevmeme,
             randomImage:url
         }
     })
+  
+  
   }
 
+
+
+  function handleChange(e){
+   const {name, value}=e.target;
+   setmeme((prevdata)=>{
+    return({
+      ...prevdata,
+      [name]:value
+    })
+   })
+  }
+
+
+// Jsx started from here...
+
   return (
-    <div className="main">
-      <div className="form">
-        <input type="text" className="form--text" placeholder="top-text" />
-        <input type="text" className="form--text" placeholder="bottom-text" />
-        <button className="form--button" onClick={generaterandomno}>
-          Get new meme image 🎇
+    <main>
+    <div className="form">
+        <input 
+            type="text"
+            placeholder="Top text"
+            className="form--input"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+        />
+        <input 
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+        />
+        <button 
+            className="form--button"
+            onClick={getMemeImage}
+        >
+            Get a new meme image 🖼
         </button>
-      </div>
-      <img src={meme.randomImage} className="memeimg" />
     </div>
+    <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+    </div>
+</main>
   );
 }
--- meme/src/components/Header.js
+++ meme/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
 
     return (
         <header className="header">
-            <img src="https://media.tenor.com/GryShD35-psAAAAM/troll-face-creepy-smile.gif" className="header--img"/>
+            <img  className="header--img"/>
             <h3 className="header--title">Project components</h3>
             <h4 className="header--project"> React project -1</h4>
         </header>
